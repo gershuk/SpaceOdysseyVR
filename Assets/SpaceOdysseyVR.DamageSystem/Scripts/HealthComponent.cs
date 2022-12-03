9239c4814baad483bc0d1b8678f097c78d251009
Vladislav Gershuk <vladik.morozov.2012@mail.ru>
2022-12-03 20:08:02 +1000

    Add damage system.

---
+++ Assets/SpaceOdysseyVR.DamageSystem/Scripts/HealthComponent.cs
@@ -0,0 +1,44 @@
+#nullable enable
+
+using System;
+
+using UnityEngine;
+
+namespace SpaceOdysseyVR.DamageSystem
+{
+    public sealed class HealthComponent : MonoBehaviour
+    {
+        public event Action OnDeath;
+
+        public event Action<uint> OnHealthChange;
+
+        [SerializeField]
+        [Range(0, 1000)]
+        private uint _health = 100;
+
+        [SerializeField]
+        [Range(0, 1000)]
+        private uint _maxHealth = 100;
+
+        public uint Health
+        {
+            get => _health;
+            set
+            {
+                if (_health == value)
+                    return;
+                _health = value;
+                OnHealthChange?.Invoke(value);
+
+                if (_health == 0)
+                {
+                    OnDeath?.Invoke();
+                }
+            }
+        }
+
+        public void TakeDamage (uint damage) => Health = Math.Max(0, Health - damage);
+
+        public void TakeHeal (uint heal) => Health = Math.Min(_maxHealth, Health + heal);
+    }
+}
